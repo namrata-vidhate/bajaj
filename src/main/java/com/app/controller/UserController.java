5299289a63ddeaec541aeced9b233774fa0b792a
namrata-vidhate <99763185+namrata-vidhate@users.noreply.github.com>
2022-04-28 20:31:15 +0530

    Update UserController.java

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.app.entity.User;
 import com.app.repository.*;
 
+@RestController
+@RequestMapping("api/bfhl")
 public class UserController {
 	
 	@Autowired
 	private UserRepository UserRepository;
 //	------------------------------------------------
-	@PostMapping("/bfhl")
+	@PostMapping("/user")
 	public User createUser(@RequestBody User user) {
 		return UserRepository.save(user);
 	}
